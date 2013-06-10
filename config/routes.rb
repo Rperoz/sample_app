4c097bac91d6a3a50d89616321111c09119a99ee
BobbyDigital <Rperoz@gmail.com>
2013-06-10 04:31:55 -0500

    Finish user edit, update, index, and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,10 @@ SampleApp::Application.routes.draw do
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+  match '/signout', to: 'sessions#destroy', via: :delete
+  match'/show', to: 'users#show'
+  match'/microposts', to: 'users#show'
   
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
