7f2f6002ff2ab13289db15ef0892339613225c14
BobbyDigital <Rperoz@gmail.com>
2013-06-17 00:22:51 -0500

    Squeaky clean and ready to deploy

--- config/routes.rb
+++ config/routes.rb
@@ -11,13 +11,14 @@ SampleApp::Application.routes.draw do
   root to: 'sessions#new'
 
   match '/home',    to: 'static_pages#home'
-  match '/signup',  to: 'sessions#new'
+  match '/signup',  to: 'users#new'
   match '/signin',  to: 'sessions#new'
   match '/signout', to: 'sessions#destroy', via: :delete
       
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+  match '/account', to: 'users#edit'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
